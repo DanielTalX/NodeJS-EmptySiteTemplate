0db678fe7761a417f4b00de94768c2ea9bdb99ba
Unknown <danielt460@gmail.com>
2018-02-19 11:31:27 +0200

    hello dani tal
    hello dani tal

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
 
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, dani!');
+    res.end('Hello, dani tal!');
 
 }).listen(process.env.PORT || 8080);
